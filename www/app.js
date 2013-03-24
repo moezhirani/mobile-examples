cf240f497e189d4b9d01f52acd61df3ec60a6733
Gabor Szabo <szabgab@gmail.com>
2013-03-25 00:52:30 +0200

    first import

---
+++ www/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var counter = 0;
+    $('#go').val(counter);
+    $('#go').click(function() {
+        counter++;
+        $('#go').val(counter);
+        return false;
+    });
+
+    $('#send').click(function() {
+        var url = 'http://perl5maven.com/search';
+        var data = {
+            "keyword" : $('#q').val()
+        };
+        $.ajax({
+            url: url,
+            data: data,
+            dataType: "json",
+            success: display_result
+        });
+        return false;
+    });
+    function display_result(data) {
+        var count = 0;
+        var html = '<ul>';
+        for (var prop in data ) {
+            count++;
+            single = '/' + prop;
+            html += '<li><a href="http://perl5maven.com/' + prop + '">';
+            html += data[prop] + '</a></li>';
+        }
+        html += '</ul>';
+        if (count == 0) {
+            html = 'No result';
+        }
+        $('#result').html(html);
+    }
+});
