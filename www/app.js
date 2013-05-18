cbc2dfb4e82f7da15370916f7a39ff49a219160c
Gabor Szabo <gabor@szabgab.com>
2013-05-18 15:41:44 +0300

    counter works again

--- www/app.js
+++ www/app.js
@@ -1,12 +1,15 @@
+function update_button(id, value) {
+    $(id).val(value);
+    $(id).prev().find('span').text(value);
+}
+
 $(document).ready(function() {
     var counter = 0;
-//    update_button('#go', counter);
-    $('#go').val(counter);
-    $("#go").prev().find('span').text(counter);
+    update_button('#go', counter);
 
     $('#go').click(function() {
         counter++;
-        $('#go').val(counter);
+        update_button('#go', counter);
         return false;
     });
 
